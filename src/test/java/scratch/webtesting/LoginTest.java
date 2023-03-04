2d622d2b40cc5649dfc96b742c7fcdd4ccf59cfc
amazingNarendra <narendrareddy9642@gmail.com>
2023-03-04 16:04:11 +0530

    first commit

---
+++ src/test/java/scratch/webtesting/LoginTest.java
@@ -0,0 +1,31 @@
+package scratch.webtesting;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LoginTest
+{
+	
+	@Test
+	public void login() 
+	{
+		String password="Narendra399@";
+	    System.setProperty("webdriver.chorme.driver", "\\Drivers\\chomeDriver\\chromedriver.exe");	
+		WebDriver d=new ChromeDriver();
+	
+//		d.manage().window().maximize();
+		d.get("https://www.amazon.com/");
+		d.findElement(By.cssSelector("a[onclick='window.location.reload()']")).click();
+		d.findElement(By.cssSelector("span[class='nav-line-2 ']")).click();
+		d.findElement(By.xpath("//input[@id='ap_email']")).sendKeys("narendrareddy9642@gmail.com");
+	    d.findElement(By.id("continue")).click();
+		d.findElement(By.xpath("//input[@id='ap_password']")).sendKeys(password);
+		d.findElement(By.className("a-button-input")).click();
+		d.findElement(By.xpath("//input[@id='twotabsearchtextbox']")).sendKeys("new shirts");
+		d.findElement(By.id("nav-search-submit-button")).click();
+		d.close();
+	}
+	
+}
